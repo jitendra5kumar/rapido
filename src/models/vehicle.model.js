84bf6c114b5e5ac57b9447ce30ecf443a3589c17
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-05-05 14:59:15 +0530

    by malik

---
+++ src/models/vehicle.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const vehicleSchema = new mongoose.Schema(
+  {
+    name: { type: String,  trim: true },
+
+    vehicle_image: String,
+    vehicle_map_icon: String,
+
+    service_id: { type: mongoose.Schema.Types.ObjectId, ref: "Service" },
+
+    slug: { type: String, unique: true },
+
+    max_seat: Number,
+    base_amount: Number,
+
+    min_per_unit_charge: Number,
+    max_per_unit_charge: Number,
+
+    min_per_min_charge: Number,
+    max_per_min_charge: Number,
+
+    min_per_weight_charge: Number,
+    max_per_weight_charge: Number,
+
+    cancellation_charge: Number,
+    waiting_time_charge: Number,
+
+    is_all_zones: { type: Boolean, default: true },
+
+    commission_type: { type: String, enum: ["percentage", "fixed"] },
+    commission_rate: Number,
+
+    created_by_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+    tax_id: { type: mongoose.Schema.Types.ObjectId, ref: "Tax" },
+
+    status: { type: String, enum: ["active", "inactive"], default: "active" },
+
+    deleted_at: { type: Date, default: null },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Vehicle", vehicleSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from './auth.routes.js';
 import userRoutes from './user.routes.js';
+import vehicletype from './vehicle.routes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
+router.use('/type',vehicletype)
 
 export default router;
